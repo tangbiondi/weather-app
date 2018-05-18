8d80ff693b8abcb5dd21768c231cb21c0d62f0a4
tangbiondi <biondi_t@hotmail.com>
2018-05-18 17:08:54 +0800

    port number changed

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.post('/', function (req, res) {
   });
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(8080, function () {
+  //console.log('Example app listening on port 3000!')
 })
